cacda70de406e91460e4f9e7cff4b8ae52211dfc
Antonio Turner <accounts@turnertechsolutions.com>
2026-06-01 11:10:58 -0400

    remove posthog recording change

--- src/index.js
+++ src/index.js
@@ -7,16 +7,7 @@ import { PostHogErrorBoundary, PostHogProvider } from '@posthog/react';
 posthog.init(process.env.REACT_APP_PUBLIC_POSTHOG_KEY, {
   api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
   defaults: '2026-01-30',
-  options={
-    api_host: 'https://us.i.posthog.com',
-    capture_pageview: true,
-    session_recording: {
-      maskAllInputs: false,        // ← unmasks all inputs
-      maskInputOptions: {
-        password: true,            // ← still masks passwords specifically
-      },
-    },
-  }
+  
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
