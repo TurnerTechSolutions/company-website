67c32ae1111783565b0abf45e9d5381a28335ab5
Antonio Turner <accounts@turnertechsolutions.com>
2026-07-12 17:57:30 -0400

    Undid changes to post hog.

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -5,8 +5,7 @@ import { AuthProvider } from '../context/AuthProvider';
 
 if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: '/ingest',
-    ui_host:  'https://us.posthog.com',
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     defaults: '2026-01-30',
     session_recording: {
       maskAllInputs: false,
